5453f5a5c798f9d9ef64f2037140a46c9ea398ec
farahdkhil <farahdkhil000@gmail.com>
2024-09-29 10:58:44 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'accueil',
+    loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule)
   }
 ];
 @NgModule({
