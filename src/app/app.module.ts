e9d83aeb3471b01f2671d68aa7c9d9ef025992b7
farahdkhil <farahdkhil000@gmail.com>
2024-10-02 20:21:50 +0100

    sign up sign in done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { AngularFireModule } from '@angular/fire/compat';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule,AngularFireModule,AngularFireAuthModule,AngularFireModule.initializeApp(environment.firebaseConfig),
+    IonicModule.forRoot(), AppRoutingModule,ReactiveFormsModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"bricolink-1817d","appId":"1:785315203083:web:9c3f94018e25d7808c5f0e","storageBucket":"bricolink-1817d.appspot.com","apiKey":"AIzaSyBQWAqXLEhpxojfCpgvAxVCvLGEgI3K1ko","authDomain":"bricolink-1817d.firebaseapp.com","messagingSenderId":"785315203083","measurementId":"G-8GFM0LYCYM"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig : {
+    apiKey: "AIzaSyBQWAqXLEhpxojfCpgvAxVCvLGEgI3K1ko",
+    authDomain: "bricolink-1817d.firebaseapp.com",
+    projectId: "bricolink-1817d",
+    storageBucket: "bricolink-1817d.appspot.com",
+    messagingSenderId: "785315203083",
+    appId: "1:785315203083:web:9c3f94018e25d7808c5f0e",
+    measurementId: "G-8GFM0LYCYM"
+}
 };
 
 /*
